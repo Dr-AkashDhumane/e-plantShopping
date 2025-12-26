8c6762bef1485c6530e410b67d7d2e1f59e7a564
Dr-AkashDhumane <djakash9766@gmail.com>
2025-12-26 06:20:52 -0500

    Completed ProductList layout and CartSlice logic

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
+// Import configureStore from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+// Import the cartReducer to manage cart state
 import cartReducer from './CartSlice';
 
+// Create the Redux store
 const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartReducer, // 'cart' slice managed by cartReducer
     },
 });
 
+// Export the store for use in the app
 export default store;
